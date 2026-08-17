d5363b4736fe6a4768e113f9c903074cb7f069a7
shadeep0918 <shadeepisuru12345@gmail.com>
2026-08-18 01:57:06 +0530

    create login service

--- src/app/features/auth/services/auth.ts
+++ src/app/features/auth/services/auth.ts
@@ -1,6 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class AuthService {
+  private http = inject(HttpClient); //httpclient connect with service
+
+  private backendUrl = 'http://localhost:8080/api/auth';  //backend URL
+
+  constructor() {
+  }
+
+  //This is login function
+  login(loginData:any){
+
+    return this.http.post(`${this.backendUrl}/login`,loginData);
+
+  }
+
+
+  register(registerData: any){
+
+    return this.http.post(`${this.backendUrl}/register`,registerData);
+  }
+
+
+}
