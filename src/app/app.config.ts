515a56a9d95a5ba3f847b5128fb109817f93c043
shadeep0918 <shadeepisuru12345@gmail.com>
2026-08-18 01:26:29 +0530

    add the build in servise what HttpClient() with call at backend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch())   //This is use for communicate with backend and it convert json file into ts or js
   ],
 };
